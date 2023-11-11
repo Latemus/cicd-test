4c3ab8c0966ad46b0be2726447c31f1fbe00b871
Lauri Viitanen <lauri_viitanen@hotmail.com>
2023-11-11 12:19:51 +0200

    Added simple test

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import unittest
+
+from bot.main import system_architecture
+
+
+class TestArchitectureDetection(unittest.TestCase):
+    def test_architecture(self):
+        """
+        Test if the get_system_architecture function returns a valid architecture string (not empty).
+        """
+        arch = system_architecture
+        self.assertTrue(arch)  # Check that the architecture is a truthy string
+
+
+if __name__ == '__main__':
+    unittest.main()
